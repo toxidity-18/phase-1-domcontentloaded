1999775693e77a5777d8d6d5e8fa777f993d0213
Samuel <samuel.kamawira@student.moringaschool.com>
2024-07-19 21:48:04 +0300

    first

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const paragraph = document.querySelector('p');
+    paragraph.textContent = 'This is really cool!';
+  });
+  
